a94bfa6d5d1bfd1132ca47404d01acd134a3ab94
Tyler Hendrickson <hendricksontyler@icloud.com>
2021-04-14 19:15:20 -0600

    refactor: removed finnish requirement for functions

--- rxjs.js
+++ rxjs.js
@@ -3,7 +3,14 @@ module.exports = {
     "plugin:rxjs/recommended"
   ],
   "rules": {
-    "rxjs/finnish": "error",
+    "rxjs/finnish": [
+      "error",
+      {
+        "functions": false,
+        "methods": false,
+        "parameters": false,
+      }
+    ],
     "rxjs/no-compat": "error",
     "rxjs/no-ignored-error": "error",
     "rxjs/no-implicit-any-catch": "off",
